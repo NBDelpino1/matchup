35397fadf85f67a9b47eb384f84338b47afc6dc1
Nicholas Delpino <NBDelpino4@gmail.com>
2017-02-28 22:36:55 -0500

    scroll cntd

--- app/public/app.js
+++ app/public/app.js
@@ -71,29 +71,27 @@ $("#submit").on("click", function(){
     return false;
 });
 
-
-
-
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
-// Scroll to specific values
-// scrollTo is the same
-window.scroll({
-    top: 2500,
-    left: 0,
-    behavior: 'smooth'
-});
 
-// Scroll certain amounts from current position
-window.scrollBy({
-    top: 100, // could be negative value
-    left: 0,
-    behavior: 'smooth'
-});
 
-// Scroll to a certain element
-document.querySelector('.hello').scrollIntoView({
-    behavior: 'smooth'
+
+
+
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
